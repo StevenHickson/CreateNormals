124c8834007b4ca6193a6431adc5b1eb71256ac7
StevenHickson <me@stevenhickson.com>
2019-07-10 11:08:10 -0400

    Updated CreateNormals to allow for two input modes

--- CreateNormals.cpp
+++ CreateNormals.cpp
@@ -9,12 +9,7 @@ using namespace std;
 using namespace pcl;
 using namespace cv;
 
-#define KINECT_CX_D 3.1304475870804731e+02
-#define KINECT_CY_D 2.4273913761751615e+02
-#define KINECT_FX_D 2.3844389626620386e+02
-#define KINECT_FY_D 5.8269103270988637e+02
-
-bool ReadParameters(string filename, vector<float> *params, vector<bool> *flat_labels) {
+bool ReadParameters(const string &filename, vector<float> *params, vector<bool> *flat_labels) {
   std::ifstream file(filename);
   string line, value;
   if(!getline(file, line))
@@ -22,17 +17,13 @@ bool ReadParameters(string filename, vector<float> *params, vector<bool> *flat_l
   istringstream ss(line);
   while(getline(ss, value, ',')) {
     params->push_back(stof(value));
-    cout << stof(value) << ",";
   }
-  cout << endl;
   if(!getline(file, line))
     return false;
   istringstream ss2(line);
   while(getline(ss2, value, ',')) {
     flat_labels->push_back(value[0] == '1');
-    cout << value << ",";
   }
-  cout << endl;
   return true;
 }
 
@@ -115,12 +106,51 @@ void EstimateNormals(const PointCloud<PointXYZ> &cloud,
 	}
 }
 
-bool OpenImage(string &filename, Mat *output) {
+bool OpenImage(const string &filename, Mat *output) {
   *output = imread(filename, IMREAD_ANYDEPTH);
   if(output->rows == 0) {
       std::cout << "Error, File: " << filename << " doesn't exist" << std::endl;
       return false;
   }
+  return true;
+}
+
+bool CreateNormals(const vector<float> &params,
+                   const vector<bool> &flat_labels,
+                   const string &depth_file,
+                   const string &labels_file,
+                   const string &output_file) {
+  PointCloud<PointXYZ> cloud;
+  PointCloud<PointNormal> normals;
+  // Open the depth file.
+  Mat depth, labels;
+  if(!OpenImage(depth_file, &depth))
+    return false;
+  // Open the labels file
+  if(!OpenImage(labels_file, &labels))
+    return false;  
+  // Convert nyu depth to float
+  depth.convertTo(depth, CV_32FC1);
+  depth = depth / 1000.0f;
+
+  // Inpaint the depth holes
+  Mat mask, mask_8bit, filled_depth;
+  threshold(depth, mask, 0, 1, THRESH_BINARY_INV);
+  mask.convertTo(mask_8bit, CV_8U);
+  inpaint(depth, mask_8bit, filled_depth, 5, INPAINT_NS);
+
+  // Create cloud and estimate normals
+  CreatePointCloud(filled_depth, params, &cloud);
+  EstimateNormals(cloud, &normals, true);
+
+  // Let's extract that data
+  Mat normal_mat;
+  GetMatFromCloud(normals, &normal_mat);
+  imwrite(output_file, normal_mat);
+
+  // Let's smooth out the flat surfaces
+  ConnectedComponents(labels, flat_labels, true, &normal_mat);
+  imwrite(labels_file + ".exr", normal_mat);
   return true;
 }
 
@@ -131,50 +161,27 @@ int main (int argc, char** argv) {
     cout << "Error reading parameters" << endl;
     return -1;
   }
-
-  std::ifstream file(argv[2]);
-  std::string str; 
-  while (std::getline(file, str)) {
-    int pos = str.find_last_of(",");
-    string depth_file = str.substr(0, pos);
-    string labels_file = str.substr(pos+1);
-    PointCloud<PointXYZ> cloud;
-    PointCloud<PointNormal> normals;
-
-    // Open the depth file.
-    Mat depth, labels;
-    if(!OpenImage(depth_file, &depth))
-      return -1;
-    // Open the labels file
-    if(!OpenImage(labels_file, &labels))
-      return -1;  
-    // Convert nyu depth to float
-    depth.convertTo(depth, CV_32FC1);
-    depth = depth / 1000.0f;
-
-    // Inpaint the depth holes
-    Mat mask, mask_8bit, filled_depth;
-    threshold(depth, mask, 0, 1, THRESH_BINARY_INV);
-    mask.convertTo(mask_8bit, CV_8U);
-    inpaint(depth, mask_8bit, filled_depth, 5, INPAINT_NS);
-
-    // Create cloud and estimate normals
-    CreatePointCloud(filled_depth, params, &cloud);
-    EstimateNormals(cloud, &normals, true);
-
-    // Let's extract that data
-    Mat normal_mat;
-    GetMatFromCloud(normals, &normal_mat);
-    imwrite(depth_file + ".exr", normal_mat);
-
-    // Let's smooth out the flat surfaces
-    timeval a,b;
-    gettimeofday(&a, 0);
-    ConnectedComponents(labels, flat_labels, true, &normal_mat);
-    gettimeofday(&b, 0);
-    std::cout << "seconds difference: " << (b.tv_sec - a.tv_sec) << std::endl;
-    std::cout << "milliseconds difference: " << (b.tv_usec - a.tv_usec) << std::endl;
-    imwrite(labels_file + ".exr", normal_mat);
+  
+  if (argc == 3) {
+    std::ifstream file(argv[2]);
+    std::string str; 
+    while (std::getline(file, str)) {
+      int pos = str.find_last_of(",");
+      string remainder = str.substr(0, pos);
+      string output_file = str.substr(pos+1);
+      pos = remainder.find_last_of(",");
+      string depth_file = remainder.substr(0, pos);
+      string labels_file = remainder.substr(pos+1);
+      if (!CreateNormals(params, flat_labels, depth_file, labels_file, output_file))
+        return -1;
+    }
+  } else if (argc == 5) {
+      if (!CreateNormals(params, flat_labels, string(argv[2]), string(argv[3]), string(argv[4])))
+        return -1;
+  } else {
+    cout << "Improper usage. Must be CreateNormals params_file text_file" << endl;
+    cout << "Or CreateNormals params_file depth_file labels_file output_file" << endl;
+    return -1;
   }
   return 0;
 }
